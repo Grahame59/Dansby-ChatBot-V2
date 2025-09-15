a39eae48b946ebe6709145c97db5061b2f1c0443
Kyler Grahame <kjgbusiness8@gmail.com>
2025-09-14 22:56:31 -0400

    Fixing Missing References in csproj file for Pipes.NLP.Mapping

--- Dansby.Shared/HandlerResult.cs
+++
@@ -1,8 +0,0 @@
-namespace Dansby.Shared
-{
-    public readonly record struct HandlerResult(bool Ok, string? ErrorCode, string? Message, object? Data)
-    {
-        public static HandlerResult Success(object? data = null) => new(true, null, null, data);
-        public static HandlerResult Fail(string code, string message) => new(false, code, message, null);
-    }
-}
